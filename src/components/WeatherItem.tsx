ac4b91492cb503003b382c66b00406c2e580354e
Artem Makushov <artemmakushov@inv.home>
2023-06-07 00:05:50 +0200

    axios setup, env setup, basic list setup

---
+++ src/components/WeatherItem.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const WeatherItem = () => {
+  return <Text>Weather</Text>;
+};
+
+export default WeatherItem;
--- src/screens/Weather.tsx
+++ src/screens/Weather.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {FlatList, SafeAreaView, Text} from 'react-native';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../navigation/types';
 
-const Weather = () => {
-  return <Text>Weather</Text>;
+type WeatherProps = NativeStackScreenProps<RootStackParamList, 'Weather'>;
+
+const Weather = ({navigation}: WeatherProps) => {
+  return (
+    <SafeAreaView>
+      {/* <FlatList
+        data={[1, 2, 3, 4]}
+        keyExtractor={item => item.id}
+        renderItem={({item, index}) => <Text>Weather</Text>}
+      /> */}
+    </SafeAreaView>
+  );
 };
 
 export default Weather;
